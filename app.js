f9eb481c2cc0ed753e2870d5ec7ccfd654689c08
Zach Retterath <ztretterath@gmail.com>
2016-10-03 12:51:54 -0400

    Require passport-local

--- app.js
+++ app.js
@@ -7,3 +7,4 @@ var hbs            = require('hbs');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var passport       = require('passport');
+var localStrategy  = require('passport-local');
