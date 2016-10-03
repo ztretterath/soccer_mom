a9cb14b02885ad0200f4f8e97617f15cadf5b5df
Zach Retterath <ztretterath@gmail.com>
2016-10-03 13:50:40 -0400

    Add userSchema data to model file

--- models/user.js
+++ models/user.js
@@ -1,2 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  username:  String,
+  password:  String,
+  createdAt: Date,
+  updatedAt: Date
+});
