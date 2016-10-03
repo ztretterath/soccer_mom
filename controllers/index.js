7dce033a4ad52479bcd3531607e198f6488cccd0
Zach Retterath <ztretterath@gmail.com>
2016-10-03 14:03:44 -0400

    Test to ensure layout.hbs is functioning

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,7 @@ var User     = require('../models/user.js');
 
 //// HOMEPAGE ROUTE ////
 router.get('/', function(req, res){
-  res.send('Home Page');
+  res.render('layout.hbs/');
 })
 
 module.exports = router;
