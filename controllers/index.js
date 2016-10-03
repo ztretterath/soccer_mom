09a648e4a2f5e6273aaa83d53efcd44915107042
Zach Retterath <ztretterath@gmail.com>
2016-10-03 15:28:08 -0400

    First commit for sign in route

--- controllers/index.js
+++ controllers/index.js
@@ -9,4 +9,20 @@ router.get('/', function(req, res){
   res.render('home.hbs/');
 })
 
+//// SIGN IN ////
+router.post('/signin',passport.authenticate('local', {failureRedirect: '/'}), function(req, res){
+  req.session.save(function(err){
+    if (err) {
+      return next(err);
+    }
+    User.findOne({username: req.session.passport.user}).exec()
+    .then(function(user){
+      res.redirect(`/home/${user._id}`);
+    })
+    .catch(function(err){
+      console.log('error: ', err);
+    })
+  })
+});
+
 module.exports = router;
