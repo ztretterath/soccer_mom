908d9b8170757f98e956dfa7ac3d54b7aa56f840
Zach Retterath <ztretterath@gmail.com>
2016-10-03 13:47:32 -0400

    Require mongoose

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ var passport       = require('passport');
 var localStrategy  = require('passport-local');
 var User           = require('./models/user.js');
 
-
+//// CONNECT TO DB ////
 mongoose.connect('mongodb://localhost/SoccerMom');
 
-//// MIDDLEWARE /////
+//// MIDDLEWARE ////
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
