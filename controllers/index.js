3b6a550095d3cd6574de9f19e30f601b9a6aa067
Zach Retterath <ztretterath@gmail.com>
2016-10-03 13:44:17 -0400

    Require user model from models folder

--- controllers/index.js
+++ controllers/index.js
@@ -2,3 +2,4 @@ var pry      = require('pryjs');
 var express  = require('express');
 var router   = express.Router();
 var passport = require('passport');
+var User     = require('./models/user.js');
