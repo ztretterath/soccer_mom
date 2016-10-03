b39847e98d0182cde1ccc550aff13557cab0a65b
Zach Retterath <ztretterath@gmail.com>
2016-10-03 12:50:42 -0400

    Require passport

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ var logger         = require('morgan');
 var hbs            = require('hbs');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var passport       = require('passport');
