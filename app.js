813d2167baa67c64b936d90d9778b5e33a3b84ae
Zach Retterath <ztretterath@gmail.com>
2016-10-03 12:49:29 -0400

    Require method-override

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-pry              = require('pryjs');
-var express      = require('express');
-var mongoose     = require('mongoose');
-mongoose.Promise = global.Promise;
-var logger       = require('morgan');
-var hbs          = require('hbs');
-var bodyParser   = require('body-parser');
+pry                = require('pryjs');
+var express        = require('express');
+var mongoose       = require('mongoose');
+mongoose.Promise   = global.Promise;
+var logger         = require('morgan');
+var hbs            = require('hbs');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
