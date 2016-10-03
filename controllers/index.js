40303bfb6a61112c63b60a465780a64c88932922
Zach Retterath <ztretterath@gmail.com>
2016-10-03 13:56:35 -0400

    Export router

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ var pry      = require('pryjs');
 var express  = require('express');
 var router   = express.Router();
 var passport = require('passport');
-var User     = require('./models/user.js');
+var User     = require('../models/user.js');
 
 //// HOMEPAGE ROUTE ////
 router.get('/', function(req, res){
   res.send('Home Page');
 })
+
+module.exports = router;
