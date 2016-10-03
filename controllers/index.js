dd969358bbf2db70b0c2b31b020abf65e3ec7cf1
Zach Retterath <ztretterath@gmail.com>
2016-10-03 13:45:23 -0400

    Add test route for home page

--- controllers/index.js
+++ controllers/index.js
@@ -3,3 +3,8 @@ var express  = require('express');
 var router   = express.Router();
 var passport = require('passport');
 var User     = require('./models/user.js');
+
+//// HOMEPAGE ROUTE ////
+router.get('/', function(req, res){
+  res.send('Home Page');
+})
