ee1694fb9d329bed6a31f29e5a5ed494aa947c82
Zach Retterath <ztretterath@gmail.com>
2016-10-04 18:21:54 -0400

    Clear users before seed

--- db/seeds.js
+++ db/seeds.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/SoccerMom');
 
-
 //// SCHEMA ////
 var Schema = require('../models/user.js');
 var User   = Schema.User;
 var Snack  = Schema.Snack;
+
+User.remove({}, function(err){
+  console.log(err);
+});
