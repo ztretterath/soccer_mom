bdf1e3da988361cc608e7e25f153f835144b7e53
Zach Retterath <ztretterath@gmail.com>
2016-10-04 18:28:48 -0400

    Begin new method for testing functionality of snacks

--- db/seeds.js
+++ db/seeds.js
@@ -1,15 +1,31 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/SoccerMom');
-
-//// SCHEMA ////
-var Schema = require('../models/user.js');
-var User   = Schema.User;
-var Snack  = Schema.Snack;
-
-User.remove({}, function(err){
-  console.log(err);
-});
-
-Snack.remove({}, function(err){
-  console.log(err);
-})
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/SoccerMom');
+//
+// //// SCHEMA ////
+// var Schema = require('../models/user.js');
+// var User   = Schema.User;
+// var Snack  = Schema.Snack;
+//
+// User.remove({}, function(err){
+//   console.log(err);
+// });
+//
+// Snack.remove({}, function(err){
+//   console.log(err);
+// })
+//
+// var cersei = new User({
+//   username: 'Cersei',
+//   snacks: [{
+//     name: 'Red Wine'
+//   }, {
+//     name: 'Pigeon Pie'
+//   }]
+// });
+//
+// var sansa = new User({
+//   username: 'Sansa',
+//   snacks: [{
+//     name: 'Lemon Cakes'
+//   }]
+// });
