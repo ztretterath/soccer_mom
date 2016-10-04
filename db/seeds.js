63a84b2c921f199f21dfb748121486253c9d3e9f
Zach Retterath <ztretterath@gmail.com>
2016-10-04 18:20:51 -0400

    Add schema and connect to db

--- db/seeds.js
+++ db/seeds.js
@@ -1,3 +1,8 @@
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/SoccerMom');
 
+
+//// SCHEMA ////
 var Schema = require('../models/user.js');
+var User   = Schema.User;
+var Snack  = Schema.Snack;
