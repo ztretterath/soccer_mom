2849db3b3720b43468d775c20f6abf8c1b9d6a18
Zach Retterath <ztretterath@gmail.com>
2016-10-04 11:18:08 -0400

    Create model section

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var passport       = require('passport');
 var localStrategy  = require('passport-local');
+
+//// MODELS ////
 var User           = require('./models/user.js');
 
 //// CONNECT TO DB ////
